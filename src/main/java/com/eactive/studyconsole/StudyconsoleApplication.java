8e0a8bca3d088f0aeabcb49c1304604965a227f8
sunwook <sunwook@eactive.co.kr>
2024-07-14 14:55:36 +0900

    take off

---
+++ src/main/java/com/eactive/studyconsole/StudyconsoleApplication.java
@@ -0,0 +1,13 @@
+package com.eactive.studyconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyconsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyconsoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eactive/studyconsole/StudyconsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eactive.studyconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyconsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
